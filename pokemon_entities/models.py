47d441ed94edf9dc28e48e23c6f87528526de0c8
zed <zed@mail.ru>
2020-10-09 00:29:06 +0300

    added fields to pok.ent model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,6 +11,16 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True )
+    appeared_at = models.DateTimeField(auto_now=True)
+    disappeared_at = models.DateTimeField(auto_now=True)
+    level = models.IntegerField(default=0)
+    health = models.IntegerField(default=0)
+    strength = models.IntegerField(default=0)
+    defence = models.IntegerField(default=0)
+    stamina = models.IntegerField(default=0)
+
+
 
     def __str__(self):
         return f"{self.lat}-{self.lon}"
